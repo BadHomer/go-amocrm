06c872b39b7452253767eae2cd4ec6af8eb23451
Vlad <vlados.it-8.company@gmail.com>
2025-11-21 14:58:00 +0300

    add user embedded and model group

---
+++ models/group.go
@@ -0,0 +1,6 @@
+package models
+
+type Group struct {
+	Name string `json:"name,omitempty"`
+	ID   int    `json:"id,omitempty"`
+}
--- models/users.go
+++ models/users.go
@@ -1,16 +1,21 @@
 package models
 
 type User struct {
-	ID                int    `json:"id"`
-	Name              string `json:"name"`
-	Email             string `json:"email"`
-	Lang              string `json:"lang"`
-	Rights            string `json:"rights"`
-	CatalogRights     string `json:"catalog_rights"`
-	CustomFieldRights string `json:"custom_field_rights"`
-	IsAdmin           bool   `json:"is_admin"`
-	IsFree            bool   `json:"is_free"`
-	IsActive          bool   `json:"is_active"`
-	GroupID           int    `json:"group_id"`
-	RoleID            int    `json:"role_id"`
+	ID                int          `json:"id,omitempty"`
+	Name              string       `json:"name,omitempty"`
+	Email             string       `json:"email,omitempty"`
+	Lang              string       `json:"lang,omitempty"`
+	Rights            string       `json:"rights,omitempty"`
+	CatalogRights     string       `json:"catalog_rights,omitempty"`
+	CustomFieldRights string       `json:"custom_field_rights,omitempty"`
+	IsAdmin           bool         `json:"is_admin,omitempty"`
+	IsFree            bool         `json:"is_free,omitempty"`
+	IsActive          bool         `json:"is_active,omitempty"`
+	GroupID           int          `json:"group_id,omitempty"`
+	RoleID            int          `json:"role_id,omitempty"`
+	Embedded          UserEmbedded `json:"_embedded,omitempty"`
+}
+
+type UserEmbedded struct {
+	Groups []Group `json:"groups,omitempty"`
 }
