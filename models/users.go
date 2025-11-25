43909a0c93259d01eaf9e1c13be2726115c5219e
Vlad <vlados.it-8.company@gmail.com>
2025-11-25 16:32:42 +0300

    rm right from user model

--- models/users.go
+++ models/users.go
@@ -1,21 +1,26 @@
 package models
 
 type User struct {
-	ID                int          `json:"id,omitempty"`
-	Name              string       `json:"name,omitempty"`
-	Email             string       `json:"email,omitempty"`
-	Lang              string       `json:"lang,omitempty"`
-	Rights            string       `json:"rights,omitempty"`
-	CatalogRights     string       `json:"catalog_rights,omitempty"`
-	CustomFieldRights string       `json:"custom_field_rights,omitempty"`
-	IsAdmin           bool         `json:"is_admin,omitempty"`
-	IsFree            bool         `json:"is_free,omitempty"`
-	IsActive          bool         `json:"is_active,omitempty"`
-	GroupID           int          `json:"group_id,omitempty"`
-	RoleID            int          `json:"role_id,omitempty"`
-	Embedded          UserEmbedded `json:"_embedded,omitempty"`
+	ID       int          `json:"id,omitempty"`
+	Name     string       `json:"name,omitempty"`
+	Email    string       `json:"email,omitempty"`
+	Lang     string       `json:"lang,omitempty"`
+	IsAdmin  bool         `json:"is_admin,omitempty"`
+	IsFree   bool         `json:"is_free,omitempty"`
+	IsActive bool         `json:"is_active,omitempty"`
+	GroupID  int          `json:"group_id,omitempty"`
+	RoleID   int          `json:"role_id,omitempty"`
+	Embedded UserEmbedded `json:"_embedded,omitempty"`
 }
 
 type UserEmbedded struct {
 	Groups []Group `json:"groups,omitempty"`
+}
+
+type RightEntity struct {
+	View   string `json:"view,omitempty"`
+	Edit   string `json:"edit,omitempty"`
+	Add    string `json:"add,omitempty"`
+	Delete string `json:"delete,omitempty"`
+	Export string `json:"export,omitempty"`
 }
